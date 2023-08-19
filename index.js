e9db5ed423282602a76c2a147d09ba4818353c80
GEOVARU <alfrediviris.1@gmail.com>
2023-08-19 09:55:06 -0600

    OK

--- index.js
+++ index.js
@@ -1,82 +1,101 @@
-// GEOVANNI RAMIREZ - 9490-20-427
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const students = {};
+const students = [];
 
-function addStudent() {
-  rl.question('Ingrese el carnet del estudiante: ', carnet => {
-    rl.question('Ingrese el nombre del estudiante: ', nombre => {
-      students[carnet] = { nombre, notas: {} };
-      addNota(carnet);
-    });
-  });
+function showMenu() {
+  console.log('Menu:');
+  console.log('1. Ingreso de Estudiante');
+  console.log('2. Ingreso de Notas');
+  console.log('3. Búsqueda por Carnet');
+  console.log('4. Generación de Resumen');
+  console.log('5. Listado de Estudiantes');
+  console.log('6. Salir');
+  rl.question('Seleccione una opción: ', handleMenu);
 }
 
-function addNota(carnet) {
-  rl.question('Ingrese el nombre de la tarea: ', tarea => {
-    rl.question(`Ingrese la nota de ${tarea} para el estudiante ${students[carnet].nombre}: `, nota => {
-      students[carnet].notas[tarea] = parseFloat(nota);
-      rl.question('¿Desea agregar otra nota para este estudiante? (si/no): ', respuesta => {
-        if (respuesta.toLowerCase() === 'si') {
-          addNota(carnet);
-        } else {
-          rl.question('¿Desea agregar notas para otro estudiante? (si/no): ', respuesta => {
-            if (respuesta.toLowerCase() === 'si') {
-              addStudent();
-            } else {
-              generateResumen();
-              rl.close();
-            }
+function handleMenu(option) {
+  switch (option) {
+    case '1':
+      rl.question('Ingrese el carnet del estudiante: ', carnet => {
+        rl.question('Ingrese el nombre del estudiante: ', nombre => {
+          students.push({ carnet, nombre, notas: [] });
+          console.log('Estudiante ingresado exitosamente.');
+          showMenu();
+        });
+      });
+      break;
+
+    case '2':
+      rl.question('Ingrese el carnet del estudiante: ', carnet => {
+        const student = students.find(s => s.carnet === carnet);
+        if (student) {
+          rl.question('Ingrese el nombre de la tarea: ', tarea => {
+            rl.question('Ingrese la nota obtenida: ', nota => {
+              student.notas.push({ tarea, nota: parseFloat(nota) });
+              console.log('Nota ingresada exitosamente.');
+              showMenu();
+            });
           });
+        } else {
+          console.log('Estudiante no encontrado.');
+          showMenu();
         }
       });
-    });
-  });
-}
+      break;
 
-function searchByCarnet() {
-  rl.question('Ingrese el carnet del estudiante que desea buscar: ', carnet => {
-    const student = students[carnet];
-    if (student) {
-      console.log(`Carnet: ${carnet}`);
-      console.log(`Nombre: ${student.nombre}`);
-      console.log('Notas:');
-      for (const tarea in student.notas) {
-        console.log(`- Tarea: ${tarea}, Nota: ${student.notas[tarea]}`);
-      }
-    } else {
-      console.log('Estudiante no encontrado.');
-    }
-    rl.close();
-  });
-}
+    case '3':
+      rl.question('Ingrese el carnet del estudiante: ', carnet => {
+        const student = students.find(s => s.carnet === carnet);
+        if (student) {
+          console.log('Estudiante encontrado:');
+          console.log('Carnet:', student.carnet);
+          console.log('Nombre:', student.nombre);
+        } else {
+          console.log('Estudiante no encontrado.');
+        }
+        showMenu();
+      });
+      break;
 
-function generateResumen() {
-  console.log('Resumen de Notas:');
-  for (const carnet in students) {
-    console.log(`Carnet: ${carnet}`);
-    console.log(`Nombre: ${students[carnet].nombre}`);
-    console.log('Notas:');
-    for (const tarea in students[carnet].notas) {
-      console.log(`- Tarea: ${tarea}, Nota: ${students[carnet].notas[tarea]}`);
-    }
-    console.log('-----------------------------------');
+    case '4':
+      console.log('Resumen de notas:');
+      students.forEach(student => {
+        student.notas.forEach(nota => {
+          console.log('Carnet:', student.carnet);
+          console.log('Nombre:', student.nombre);
+          console.log('Tarea:', nota.tarea);
+          console.log('Nota:', nota.nota);
+          console.log('-------------------------');
+        });
+      });
+      showMenu();
+      break;
+
+    case '5':
+      console.log('Listado de estudiantes:');
+      students.forEach(student => {
+        console.log('Carnet:', student.carnet);
+        console.log('Nombre:', student.nombre);
+        console.log('-------------------------');
+      });
+      showMenu();
+      break;
+
+    case '6':
+      console.log('¡Hasta luego!');
+      rl.close();
+      break;
+
+    default:
+      console.log('Opción no válida. Seleccione una opción del menú.');
+      showMenu();
+      break;
   }
 }
 
-rl.question('¿Qué acción desea realizar? (agregar/buscar/resumen/salir): ', respuesta => {
-  if (respuesta === 'agregar') {
-    addStudent();
-  } else if (respuesta === 'buscar') {
-    searchByCarnet();
-  } else if (respuesta === 'resumen') {
-    generateResumen();
-    rl.close();
-  } else if (respuesta === 'salir') {
-    rl.close();
-  }
-});
+showMenu();
